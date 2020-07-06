eaa7b6bbf4f5d4f91f277bc7a82dd324c29477f3
wangqianqian <1848105094@qq.com>
2020-07-07 00:04:36 +0800

    登录页面接口对接

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,23 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { 
+    Button, 
+    FormItem,
+    Form,
+    Input,
+    Message
+    
+
+} from 'element-ui'
 
 Vue.use(Button)
+Vue.use(FormItem)
+Vue.use(Form)
+Vue.use(Input)
+
+Message.install = function (Vue, options) {
+    Vue.prototype.$message = Message
+    // vue其他的实例就可以引用这个$message方法
+  }
+  Vue.use(Message )//消息提示
+//  引入组件的时候自动调用Vue.use()方法，Message对象又会自动调用install这个方法
+// 把这个Message对象放在vue的原型的上。
