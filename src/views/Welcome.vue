81c4b9475400eec04c5a485cbaf62b3a5507cb41
wangqianqian <wangqianqian@qq.com>
2020-07-07 23:41:52 +0800

    侧边栏渲染及相应特效

---
+++ src/views/Welcome.vue
@@ -0,0 +1,24 @@
+<!-- 首页Welcome组件 -->
+<template>
+  <div>这里是welcome组件</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    };
+  },
+
+  components: {},
+
+  computed: {},
+
+  mounted() {},
+
+  methods: {}
+}
+
+</script>
+<style  scoped>
+</style>
